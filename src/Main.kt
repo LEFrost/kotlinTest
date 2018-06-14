340ab74b632b5ee537324cf8941308808a54edcb
LEFrost <pengjian@live.cn>
2018-06-14 16:38:22 +0800

    删除main.java，自己实现模糊匹配

--- src/Main.kt
+++ src/Main.kt
@@ -7,11 +7,11 @@ fun main(args: Array<String>) {
             Data(1, "10628888", "xw", true, false),
             Data(2, "1062888801", "xw", false, false),
             Data(3, "1062888801", "xw1", false, true),
-            Data(4, "10628888", "xw1", true, true),
+            Data(4, "10628888", "01xw", true, true),
             Data(5, "10628888", null, false, false)
     )
     val accessNo = readLine()?:""
     val featureStr = readLine()?:""
     val resutl = list.matchAccessNo(accessNo).matchFeatureStr(featureStr)
-    println(resutl?.seq ?: "匹配失败!!!")
+    println("查询结果:${resutl?.seq ?: "匹配失败!!!"}")
 }
